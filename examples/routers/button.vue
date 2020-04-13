948a69ffba5d2f90d849dc3ac8a96ebb2c257446
AllenChinese <13402202685@163.com>
2020-04-13 11:47:19 +0800

    feat: Add Ac Button Component

--- examples/routers/button.vue
+++ examples/routers/button.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <ac-button></ac-button>
+    <h3>type</h3>
+    <ac-button type="default">default</ac-button>
+    <ac-button type="primary">primary</ac-button>
+    <ac-button type="text">text</ac-button>
+    <ac-button type="info">info</ac-button>
+    <ac-button type="success">success</ac-button>
+    <ac-button type="warning">warning</ac-button>
+    <ac-button type="error">error</ac-button>
+    <h3>size</h3>
+    <ac-button type="primary" size="large">Large</ac-button>
+    <ac-button type="primary" size="default">Default</ac-button>
+    <ac-button type="primary" size="small">Small</ac-button>
   </div>
 </template>
 
--- src/components/button/src/button.vue
+++ src/components/button/src/button.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="button">按钮</div>
+  <div :class="classes">
+    <span><slot ref="slot"></slot></span>
+  </div>
 </template>
 
 <script>
+const prefixCls = "ac-btn";
 export default {
-  name: "AcButton"
+  name: "AcButton",
+  props: {
+    /** 按钮类型 */
+    type: {
+      type: String,
+      default: "default"
+    },
+    /** 按钮形状 */
+    shape: {
+      type: String,
+      default: ""
+    },
+    /** 按钮尺寸 */
+    size: {
+      type: String,
+      default: "default"
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        `${prefixCls}`,
+        `${prefixCls}-${this.type}`,
+        {
+          [`${prefixCls}-${this.shape}`]: !!this.shape,
+          [`${prefixCls}-${this.size}`]: this.size !== "default"
+        }
+      ];
+    }
+  }
 };
 </script>
